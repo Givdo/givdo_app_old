d99f289c8e33d0315e0376e651d1f05932564aaf
Carlos Palhares <chjunior@gmail.com>
2015-10-22 18:21:05 -0200

    Remove dead code

--- specs/spec-helper.js
+++ specs/spec-helper.js
@@ -1,16 +1,6 @@
 'use strict';
 
-beforeEach(module('givdo', function ($provide) {
-  // $provide.factory("$ionicModal", ['$q', function ($q) {
-  //   var deferModal = $q.defer();
-  //   var fromTemplateUrlStub = jasmine.createSpy().and.returnValue(deferModal.promise);
-
-  //   return {
-  //     resolve: deferModal.resolve,
-  //     fromTemplateUrl: fromTemplateUrlStub
-  //   };
-  // }]);
-}));
+beforeEach(module('givdo'));
 
 beforeEach(inject(function ($httpBackend) {
   $httpBackend.whenGET(/templates.*/).respond(200, '');
