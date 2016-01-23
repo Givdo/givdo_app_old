3e57e4eb53c2d6de8e78d5ec15d792d4eaab459f
Carlos Palhares <chjunior@gmail.com>
2016-01-22 22:18:46 -0200

    Setup givdo-qa as the default environment

--- www/js/givdo.config.js
+++ www/js/givdo.config.js
@@ -2,5 +2,5 @@
   'use strict';
 
   angular.module('givdo.config', [])
-    .constant('GivdoApiURL', 'http://localhost:3000/api/v1');
+    .constant('GivdoApiURL', 'http://givdo-qa.herokuapp.com/api/v1');
 })();
