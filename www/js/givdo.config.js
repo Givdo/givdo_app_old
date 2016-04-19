856ef92c83c9376d676ebf18373e6e84ce6c6793
Diego Toral <diegotoral@gmail.com>
2016-04-19 17:44:22 -0300

    Revert configuration to use staging API

--- www/js/givdo.config.js
+++ www/js/givdo.config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular.module('givdo.config', [])
-    .constant('GivdoApiURL', 'http://localhost:3000/api/v1');
+    .constant('GivdoApiURL', 'http://givdo-qa.herokuapp.com/api/v1');
 
 })();
