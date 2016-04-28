eb4adb072682eaefa589633f1a248302af85d418
Hudson Marinho <hudsonmarinhodev@gmail.com>
2016-04-28 18:23:36 -0300

    Removed spaces and comments

--- www/js/givdo.js
+++ www/js/givdo.js
@@ -4,14 +4,12 @@
   angular.module('givdo', ['ionic', 'givdo.auth', 'givdo.quiz', 'givdo.user', 'givdo.ui'])
     .run(['$ionicPlatform', 'authLock', function ($ionicPlatform, authLock) {
       $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
         if (window.cordova && window.cordova.plugins.Keyboard) {
           cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
           cordova.plugins.Keyboard.disableScroll(true);
         }
+
         if (window.StatusBar) {
-          // org.apache.cordova.statusbar required
           StatusBar.styleDefault();
         }
 
