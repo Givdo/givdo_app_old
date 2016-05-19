0d71d3d89fdee0523e01e0ff40e16049c66f57e9
Diego Toral <diegotoral@gmail.com>
2016-05-18 21:15:57 -0300

    Re-add ios platform

--- www/js/core/config.js
+++ www/js/core/config.js
@@ -5,10 +5,12 @@
     .module('givdo.core')
     .config(['facebookConfig', '$cordovaFacebookProvider', configurePlugins])
     .config(['$httpProvider', configureSerializer])
-    .run(['$ionicPlatform', 'localStorageService', configurePlatform]);
+    .run(['$ionicPlatform', configurePlatform]);
 
 
   function configurePlugins(facebookConfig, $facebookProvider) {
+    if (window.cordova) return;
+
     $facebookProvider.browserInit(facebookConfig.appID, facebookConfig.version);
   }
 
@@ -16,7 +18,7 @@
     $httpProvider.defaults.paramSerializer = '$httpParamSerializerJQLike';
   }
 
-  function configurePlatform($ionicPlatform, localStorage) {
+  function configurePlatform($ionicPlatform) {
     $ionicPlatform.ready(function () {
       if (window.cordova && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -26,8 +28,6 @@
       if (window.StatusBar) {
         StatusBar.styleDefault();
       }
-
-      console.log('Token:' + localStorage.get('token'));
     });
   }
 })();
