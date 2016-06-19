92f70a234639aa3e4b87693f28c90e0026016d39
Hudson Marinho <hudsonmarinhodev@gmail.com>
2016-06-19 20:20:09 -0300

    Changed action notifications for get current page

--- www/js/api/user.repository.js
+++ www/js/api/user.repository.js
@@ -3,10 +3,10 @@
 
   angular
     .module('givdo.api')
-    .factory('UserRepository', ['GivdoApiURL', 'repository', UserRepository]);
+    .factory('UserRepository', ['GivdoApiURL', 'repository', 'transport', UserRepository]);
 
 
-    function UserRepository(baseUrl, Repository) {
+    function UserRepository(baseUrl, Repository, transport) {
       var repository = Repository({
         friends: {
           method: 'GET',
@@ -20,15 +20,15 @@
         causes: {
           method: 'POST',
           url: baseUrl + '/causes',
-        },
-        notifications: {
-          method: 'GET',
-          url: baseUrl + '/notifications',
         }
       });
 
       repository.update = function (user, data) {
         return user.load('self', { data: data, method: 'PATCH' });
+      }
+
+      repository.notifications = function (page) {
+        return transport.load(baseUrl + '/notifications' + page, { method: 'GET' });
       }
 
       return repository;
