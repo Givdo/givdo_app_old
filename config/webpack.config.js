0b4abb4d766d60e61f12438c7d0d5443bda8c93d
Diego Toral <diegotoral@gmail.com>
2017-05-05 19:34:14 -0300

    Update webpack configuration to load environment specific config

---
+++ config/webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var projectRootDir = process.env.IONIC_ROOT_DIR;
+var appScriptsDir = process.env.IONIC_APP_SCRIPTS_DIR;
+
+var env = process.env.IONIC_ENV || 'dev';
+var config = require(path.join(appScriptsDir, 'config', 'webpack.config.js'));
+
+config.resolve = config.resolve || {};
+config.resolve.alias = {
+  'config$': path.resolve(projectRootDir, 'src', 'config', env),
+};
+
+config.plugins = config.plugins || [];
+config.plugins.push(
+  new webpack.DefinePlugin({
+    ENV: env,
+  })
+);
+
+module.exports = config;
