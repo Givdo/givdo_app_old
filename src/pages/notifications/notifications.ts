465e06014adfe2d6b9884073fed89753b62b2d70
Diego Toral <diegotoral@gmail.com>
2017-05-20 09:37:50 -0300

    Use NotificationItem component on notifications page

--- src/pages/notifications/notifications.ts
+++ src/pages/notifications/notifications.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the Notifications page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-notifications',
@@ -27,16 +21,7 @@ export class NotificationsPage {
     { id: 10, name: 'Hudson Marinho', sender_image: 'https://scontent.fnat1-1.fna.fbcdn.net/v/t1.0-9/15135885_10211374182932589_8708865600205752698_n.jpg?oh=de2eb36822682431284320c218e2b7e8&oe=59B7F8B8', category: "Other" },
   ];
 
-  accept(item: string) {
-    console.log('Accept', item);
-  }
-
-  reject(item: string) {
-    console.log('Reject', item);
-  }
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Notifications');
