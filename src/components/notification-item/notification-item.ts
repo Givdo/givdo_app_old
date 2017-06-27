f151e09060021a170fdca8eb4f386d35eb3ff267
Hudson Marinho <hudsonmarinhodev@gmail.com>
2017-06-27 01:27:11 -0300

    Update notification-item

--- src/components/notification-item/notification-item.ts
+++ src/components/notification-item/notification-item.ts
@@ -10,8 +10,10 @@ import {
   selector: 'notification-item',
   templateUrl: 'notification-item.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  inputs: ['buttons']
 })
 export class NotificationItemComponent {
+  @Input() buttons: boolean = false;
   @Input() notification = {};
   @Output() accept = new EventEmitter<number>();
   @Output() reject = new EventEmitter<number>();
