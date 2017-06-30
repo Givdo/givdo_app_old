b21c3fc3b01e617787a17ca96c62cb1ae4625ac1
Hudson Marinho <hudsonmarinhodev@gmail.com>
2017-06-30 00:32:31 -0300

    Create page activity

--- src/pages/activity/activity.ts
+++ src/pages/activity/activity.ts
@@ -13,12 +13,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'activity.html',
 })
 export class ActivityPage {
+  score: number = 0;
   notifications = [
-    { id: 1, name: 'Breast Cancer Emergency Found(BCEF)',    sender_image: 'http://placehold.it/50x50', category: "Other" },
-    { id: 2, name: 'Marine Conservation Science Institute',  sender_image: 'http://placehold.it/50x50', category: "Other" },
-    { id: 3, name: 'Equality California',                    sender_image: 'http://placehold.it/50x50', category: "Other" },
-    { id: 3, name: 'Equality California',                    sender_image: 'http://placehold.it/50x50', category: "Other" },
-    { id: 4, name: 'Sierra Club',                            sender_image: 'http://placehold.it/50x50', category: "Other" },
+    { id: 1, name: 'Breast Cancer Emergency Found(BCEF)',    sender_image: 'http://placehold.it/50x50', score: 10 },
+    { id: 2, name: 'Marine Conservation Science Institute',  sender_image: 'http://placehold.it/50x50', score: 1 },
+    { id: 3, name: 'Equality California',                    sender_image: 'http://placehold.it/50x50', score: 18 },
+    { id: 3, name: 'Equality California',                    sender_image: 'http://placehold.it/50x50', score: 2 },
+    { id: 4, name: 'Sierra Club',                            sender_image: 'http://placehold.it/50x50', score: 12 },
+    { id: 5, name: 'Breast Cancer Emergency Found(BCEF)',    sender_image: 'http://placehold.it/50x50', score: 7 },
+    { id: 6, name: 'Marine Conservation Science Institute',  sender_image: 'http://placehold.it/50x50', score: 7 },
+    { id: 7, name: 'Equality California',                    sender_image: 'http://placehold.it/50x50', score: 5 },
+    { id: 8, name: 'Equality California',                    sender_image: 'http://placehold.it/50x50', score: 10 },
+    { id: 9, name: 'Sierra Club',                            sender_image: 'http://placehold.it/50x50', score: 11 },
   ];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
@@ -28,7 +34,13 @@ export class ActivityPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad Activity');
+    let score: number = this.score;
+
+    this.notifications.forEach(function(e){
+      score += e.score
+    });
+
+    this.score = (score / 100)
   }
 
 }
