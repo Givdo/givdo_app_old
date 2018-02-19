f2cf565da9a23290a72e2a219de972400c11d3ca
Smita Adhav <adhavsmita.asi@gmail.com>
2018-02-19 12:08:59 -0500

    Added search button and a slider for Organization modal page

--- src/pages/organization-modal/organization-modal.ts
+++ src/pages/organization-modal/organization-modal.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, ViewController, NavController, NavParams } from 'ionic-angular';
+import { OrganizationService } from '../../services/organization.service';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
 
 @IonicPage()
 @Component({
@@ -7,55 +14,82 @@ import { IonicPage, ViewController, NavController, NavParams } from 'ionic-angul
   templateUrl: 'organization-modal.html',
 })
 export class OrganizationModalPage {
-  organizations = [
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' },
-    { name: "Organization", image: 'http://via.placeholder.com/150x150' }
-  ]
-
-  constructor(public viewCtrl: ViewController, public navCtrl: NavController, public navParams: NavParams) {
-  }
+//profileName : any;
+organization : any;
+searchQuery: string = '';
+errorMessage: string;
+private organizations : any [] =[];
+private items : any;
+haveData: boolean = false;
+  
+@ViewChild(Slides) slides: Slides;
+  constructor( public viewCtrl: ViewController,
+               public navCtrl: NavController, 
+               public navParams: NavParams, 
+               public _organizationService: OrganizationService) 
+               {}
 
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad OrganizationModalPage');
-    // this.profile = this.viewCtrl.data;
+   this._organizationService.getOrganizations()
+                  .subscribe( organizations => {
+                  this.organizations = organizations['data'],
+                  error => this.errorMessage = <any>error,
+                  () => console.log('done')
+                });
   }
 
   closeModal() {
-    this.viewCtrl.dismiss();
+      this.viewCtrl.dismiss(this.organization);
   }
 
-  goback() {
-    this.closeModal();
+  onOrganizationSelected($event, organizationSelected) {
+      this.organization = organizationSelected;
+  }
+
+  initializeItems() {
+     this.items = this.organizations;
+  }
+
+  getItems(event: any) {
+      // Reset items back to all of the items
+      this.initializeItems();
+      // set val to the value of the searchbar
+       let val = event.target.value;
+      // if the value is an empty string don't filter the items
+        if (val && val.trim() != '') {
+          this.haveData = true;
+          this.items = this.items.filter((orgList) => {
+          return (orgList.attributes.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+          })
+        } else {
+          console.log('Nothing to list');
+          this.haveData = false;
+        }
+  }
+    
+  slideChanged() {
+    if (this.slides.isEnd()) {
+        this.slideToFirst();
+    }
+    if(this.slides.isBeginning()) {
+       this.slideNext();
+    }
+  }
+  
+  getNext() {
+    this.slides.slideNext(500, true);
+  }
+
+  getPrev() {
+    this.slides.slidePrev(500, true);
+  }
+
+  slideNext() {
+    this.getNext();
+  }
+
+  slideToFirst() {
+    this.slides.slideTo(0, 100);
   }
 }
