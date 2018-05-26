1d884c48528a178bee137e6e36734197f338bccc
Takashi <tlab241@gmail.com>
2018-05-26 15:46:42 +0900

    Display the page to select mode &  Show the seriese of quizzes

--- src/pages/quiz/quiz.ts
+++ src/pages/quiz/quiz.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular';
 
 import { Quiz } from "./quiz.model"
 import { quizService } from '../../providers/quizService';
@@ -10,52 +10,60 @@ import { quizService } from '../../providers/quizService';
   templateUrl: 'quiz.html',
 })
 export class QuizPage {
-  quiz = true;
+  
+  //Variables to switch views
+  showQuiz = false;
+  singlePlayerMode: boolean;
+  multiPlayerMode: boolean;
   buttons = false;
   next_question = false;
   sponsored = false;
   winner = false;
   lost = false;
-  
+
+  //Variables to control quizzes
   quizzes: Quiz[];
+  currentQuiz: Quiz;
+  currentQuizId: number;
   errMess: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-              private quizService: quizService) {
-  }
+  constructor( private quizService: quizService) {}
 
   ionViewDidLoad() {
-    let $self = this;
 
-    setTimeout(function() {
-      $self.buttons = true;
-      $self.next_question = true;
-    }, 12000);
-
+    // Load quizzes
     this.quizService.getQuizzes()
-      .subscribe(quizzes => this.quizzes = quizzes["data"],
+        .subscribe(quizzes => this.quizzes = quizzes["data"],
                   error => this.errMess = error,
-                  () => console.log("done getQuizzes()"))
+                  () => {console.log("done getQuizzes()");
+                         this.currentQuiz = this.quizzes.shift();})
+    this.buttons = true;
+    this.next_question = true;
   }
 
+  //Start quiz on the single player mode 
   startSingleplay(){
-    this.quiz = true;
+    this.showQuiz = true;
+    this.singlePlayerMode = true;
   }
 
+  //Start quiz on the multi player mode
+  //So far, this function doesn't do anything
   startMultiplayer(){
-    this.quiz = true;
+    this.multiPlayerMode = true;
   }
 
   buttonQuestion(){
     this.buttons = true;
   }
 
-  quizConfirm(){
-    this.next_question = true;
-  }
-
   quizNext(){
-    this.sponsored = true;
-    this.quiz = false;
+    this.currentQuizId = this.currentQuizId + 1;
+    if (this.quizzes.length != 0){
+      this.currentQuiz = this.quizzes.shift()
+    }else{
+      this.showQuiz = false;
+      this.sponsored = true;
+    }
   }
 }
